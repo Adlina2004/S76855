c4f380e276f5e8fac56c98b2cd76d743cab8ecdd
user <user@172.17.182.143>
2024-11-10 10:39:58 +0800

    This is my first commit for lab 2 and lab 3 question 1

---
+++ Adlina2004/src/Lab2/L2q2.java
@@ -0,0 +1,13 @@
+
+package Lab2;
+
+public class L2q2 {
+       public static void main(String[] args) {
+        // output
+        
+        System.out.println("Siti Aisyah Humayraa");
+        System.out.println("School of Informatics and Applied Mathematics");
+        System.out.println("Software Engineering");
+    }
+    
+}
